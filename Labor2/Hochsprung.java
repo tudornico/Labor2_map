632ee7e933186ff4c950413bd0dfa705379638b8
tudornico <46244937+tudornico@users.noreply.github.com>
2021-10-21 13:19:00 +0300

    Update Hochsprung.java

--- Labor2/Hochsprung.java
+++ Labor2/Hochsprung.java
@@ -4,6 +4,9 @@ import Labor2.Leichtathletik;
 
 public class Hochsprung extends Leichtathletik {
     @Override
+    /**
+    *returns the time needed for the sport
+     */
     public double kalkuliereZeit() {
         return 20;
     }
