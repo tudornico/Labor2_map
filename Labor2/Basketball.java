4c338e612ec0ecf6418f6aaf804fd7d968cf7ea8
tudornico <46244937+tudornico@users.noreply.github.com>
2021-10-21 13:15:42 +0300

    Update Basketball.java

--- Labor2/Basketball.java
+++ Labor2/Basketball.java
@@ -2,6 +2,9 @@ package Labor2;
 
 public class  Basketball extends Mannschaftsport {
     @Override
+    /**
+     * returns the time nedded for the sport
+     */
     public double kalkuliereZeit(){
         return 55;
     }
