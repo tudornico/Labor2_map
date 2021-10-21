a6a81617d51ad3acc10e928e29b0e9433b1578d0
tudornico <46244937+tudornico@users.noreply.github.com>
2021-10-21 13:18:26 +0300

    Update Hindernslauf.java

--- Labor2/Hindernslauf.java
+++ Labor2/Hindernslauf.java
@@ -2,6 +2,9 @@ package Labor2;
 
 public class Hindernslauf extends Leichtathletik {
     @Override
+    /**
+     * returns the time needed for the sport
+     */
     public double kalkuliereZeit() {
         return 30;
     }
